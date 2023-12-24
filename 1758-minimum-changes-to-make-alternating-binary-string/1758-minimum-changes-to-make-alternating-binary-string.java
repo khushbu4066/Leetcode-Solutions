88f2052befa5f5f770f129b83e6c3d8f02061ae1
CodeGeek <khushbudadhe732@gmail.com>
2023-12-24 15:31:29 +0530

    Time: 3 ms (92.13%), Space: 42.4 MB (7.87%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -3,28 +3,48 @@
 //S.C : O(n)
 public class Solution {
     public int minOperations(String s) {
-        int n = s.length();
-        int start_with_0 = 0;
-        int start_with_1 = 0;
+//         int n = s.length();
+//         int start_with_0 = 0;
+//         int start_with_1 = 0;
 
-        // 010101
-        // 101010
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                if (s.charAt(i) == '0') {
-                    start_with_1++;
-                } else {
-                    start_with_0++;
+//         // 010101
+//         // 101010
+//         for (int i = 0; i < n; i++) {
+//             if (i % 2 == 0) {
+//                 if (s.charAt(i) == '0') {
+//                     start_with_1++;
+//                 } else {
+//                     start_with_0++;
+//                 }
+//             } else {
+//                 if (s.charAt(i) == '1') {
+//                     start_with_1++;
+//                 } else {
+//                     start_with_0++;
+//                 }
+//             }
+//         }
+
+//         return Math.min(start_with_0, start_with_1);
+        
+        
+        
+        int n = s.length();
+        int start_with_0 = 0; //010101
+        
+        for(int i = 0; i<n; i++){
+            if(i%2==0){
+                if(s.charAt(i)=='0'){
+                    start_with_0++;                
                 }
-            } else {
-                if (s.charAt(i) == '1') {
-                    start_with_1++;
-                } else {
+            }
+            else{
+                if(s.charAt(i)=='1'){
                     start_with_0++;
                 }
             }
         }
-
+        int start_with_1 = n - start_with_0;
         return Math.min(start_with_0, start_with_1);
     }
 }
