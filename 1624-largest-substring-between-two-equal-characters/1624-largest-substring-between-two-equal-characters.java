089741827265c02254e23909ed9f60cbd2917ee1
CodeGeek <khushbudadhe732@gmail.com>
2023-12-31 13:33:07 +0530

    Time: 3 ms (37.34%), Space: 41.5 MB (11.53%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
@@ -1,16 +1,36 @@
 class Solution {
     public int maxLengthBetweenEqualCharacters(String s) {
         
-       int n = s.length();
-        int result = -1;
+//        int n = s.length();
+//         int result = -1;
         
-        for(int i = 0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                if(s.charAt(i)==s.charAt(j)){
-                result = Math.max(result, j-i-1);
-                }
+//         for(int i = 0; i<n; i++){
+//             for(int j = i+1; j<n; j++){
+//                 if(s.charAt(i)==s.charAt(j)){
+//                 result = Math.max(result, j-i-1);
+//                 }
+//             }
+//         }
+//         return result;
+        
+        int n = s.length();
+        HashMap<Character, Integer> mp = new HashMap<>();
+
+        int result = -1;
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+
+            if (!mp.containsKey(ch)) {
+                mp.put(ch, i);
+            } else {
+                result = Math.max(result, i - mp.get(ch) - 1);
             }
         }
+
         return result;
     }
 }
+
+// TC = O(n^2)
+// SC = O(1)
