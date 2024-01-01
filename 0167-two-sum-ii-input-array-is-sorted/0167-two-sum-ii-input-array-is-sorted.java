d53c189b98dc140cb82e64c0feb32ca3376cabaa
CodeGeek <khushbudadhe732@gmail.com>
2024-01-02 04:55:17 +0530

    Time: 7 ms (10.61%), Space: 46.2 MB (33.49%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+//         int len =  numbers.length;
+//         int[] result = new int [2];
+//         int number1 = 0;
+//         int sum = 0;
+    
+//     for(int i = 0; i<len; i++);{
+//     number1 = numbers[i];
+
+//     for(int j = i+1; i<len; j++);{
+//     sum = number1 + numbers[j];
+    
+//     if (sum ==target){
+//         result[0] = i;
+//         result[1] = j;
+//     }
+//     }
+//     return result;
+//     }
+        
+        HashMap<Integer,Integer> set = new HashMap<>();
+        
+        int n = arr.length;
+        
+        for(int i = 0; i<n; i++){
+            int complement = target - arr[i];
+            
+            if(set.containsKey(complement)){
+                return new int[]{set.get(complement)+1,i+1};
+            }
+            set.put(arr[i], i);
+        }
+        return new int[]{};
+    }
+}
