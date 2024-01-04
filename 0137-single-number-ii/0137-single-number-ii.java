9166f28c319243534c7c14661168f86f65635b25
CodeGeek <khushbudadhe732@gmail.com>
2024-01-04 15:18:13 +0530

    Time: 4 ms (60.12%), Space: 45.4 MB (15.69%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,12 +1,25 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        int x = 0;
-        int y = 0;
-        for(int i = 0; i<nums.length; i++){
-            x = (x^nums[i]) & ~y;
-            y = (y^nums[i]) & ~x;
+    public int singleNumber(int[] arr) {
+        // int x = 0;
+        // int y = 0;
+        // for(int i = 0; i<nums.length; i++){
+        //     x = (x^nums[i]) & ~y;
+        //     y = (y^nums[i]) & ~x;
+        // }
+        // return x;
+        
+       
+        Arrays.sort(arr);
+         int n = arr.length;
+        for(int i = 0; i<n; i+=3){
+            if(i+1 >= n || i+2 >= n){
+                return arr[i];
+            }
+            if(arr[i] != arr[i+1] || arr[i] != arr[i+2]){
+                return arr[i];
+            }
         }
-        return x;
+        return -1;
     }
 }
 
