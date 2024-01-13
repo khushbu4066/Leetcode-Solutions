08fbd30351b292f1fbcd3706986d1f76d773360f
CodeGeek <khushbudadhe732@gmail.com>
2024-01-13 13:11:10 +0530

    Time: 8 ms (76.51%), Space: 45.4 MB (18.05%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,24 +1,44 @@
+// class Solution {
+//     public int minSteps(String s, String t) {
+        
+//         int n = s.length();
+//         int[] ans_s = new int[26];
+//         int[] ans_t = new int[26];
+        
+//         for(int i = 0; i<n; i++){
+//             ans_s[s.charAt(i)-'a']++;
+//             ans_t[t.charAt(i)-'a']++;
+//         }
+//         int result = 0;
+        
+//         for(int i = 0; i<26; i++){
+            
+//             int freqOfS = ans_s[i];
+//             int freqOfT = ans_t[i];
+            
+//             if(freqOfT < freqOfS){
+//                 result += freqOfS - freqOfT;
+//             }
+//         }
+            
+//             return result;
+//     }
+// }
+
 class Solution {
     public int minSteps(String s, String t) {
         
         int n = s.length();
-        int[] ans_s = new int[26];
-        int[] ans_t = new int[26];
+        int[] ans = new int[26];
         
         for(int i = 0; i<n; i++){
-            ans_s[s.charAt(i)-'a']++;
-            ans_t[t.charAt(i)-'a']++;
+            ans[s.charAt(i)-'a']++;
+            ans[t.charAt(i)-'a']--;
         }
         int result = 0;
         
         for(int i = 0; i<26; i++){
-            
-            int freqOfS = ans_s[i];
-            int freqOfT = ans_t[i];
-            
-            if(freqOfT < freqOfS){
-                result += freqOfS - freqOfT;
-            }
+            result += Math.max(0, ans[i]);
         }
             
             return result;
