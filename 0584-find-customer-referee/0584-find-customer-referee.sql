940fbea4b765e05352a8e61255ca6ad836fd7bee
CodeGeek <khushbudadhe732@gmail.com>
2024-01-20 21:53:48 +0530

    Time: 872 ms (61.72%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -3,4 +3,7 @@ name
 FROM 
 Customer
 WHERE 
-referee_id is NULL OR referee_id != 2
+#referee_id is NULL OR referee_id != 2
+
+IFNULL(referee_id,0)<>2  
+# Another way to write the query using function IFNULL and <> means != 
