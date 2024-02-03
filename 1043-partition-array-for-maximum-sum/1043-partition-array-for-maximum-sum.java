fa2be236326295c7f283ec8cef099e6141f1babb
CodeGeek <khushbudadhe732@gmail.com>
2024-02-03 13:14:51 +0530

    Time: 6 ms (93.17%), Space: 41.8 MB (77.66%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,31 +1,52 @@
+// class Solution {
+//     private int n;
+//     private int[] t;
+
+//     public int maxSumAfterPartitioning(int[] arr, int k) {
+//         n = arr.length;
+//         t = new int[501];
+//         Arrays.fill(t, -1);
+//         return solve(arr, 0, k);
+//     }
+
+//     private int solve(int[] arr, int idx, int k) {
+//         if (idx >= n) {
+//             return 0;
+//         }
+
+//         if (t[idx] != -1) {
+//             return t[idx];
+//         }
+
+//         int result = 0;
+//         int currMax = -1;
+//         for (int i = idx; i < n && i - idx + 1 <= k; i++) {
+//             currMax = Math.max(currMax, arr[i]);
+
+//             result = Math.max(result, currMax * (i - idx + 1) + solve(arr, i + 1, k));
+//         }
+
+//         return t[idx] = result;
+//     }
+// }
+
 class Solution {
-    private int n;
-    private int[] t;
 
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        n = arr.length;
-        t = new int[501];
-        Arrays.fill(t, -1);
-        return solve(arr, 0, k);
-    }
-
-    private int solve(int[] arr, int idx, int k) {
-        if (idx >= n) {
-            return 0;
+        
+        int n = arr.length;
+        int[] t = new int[n+1];
+        
+        for(int i = 1; i<=n; i++){
+            
+            int currMax = -1;
+            //int result = 0;
+            
+            for(int j = 1; j<=k && i-j >= 0; j++){
+                currMax = Math.max(currMax, arr[i-j]);
+               t[i] = Math.max(t[i], t[i-j] + currMax * j);
+            }
         }
-
-        if (t[idx] != -1) {
-            return t[idx];
-        }
-
-        int result = 0;
-        int currMax = -1;
-        for (int i = idx; i < n && i - idx + 1 <= k; i++) {
-            currMax = Math.max(currMax, arr[i]);
-
-            result = Math.max(result, currMax * (i - idx + 1) + solve(arr, i + 1, k));
-        }
-
-        return t[idx] = result;
+        return t[n];
     }
 }
