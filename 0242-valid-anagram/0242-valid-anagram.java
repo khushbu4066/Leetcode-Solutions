f74c2e30af296bbfb8b6ca1666a303f4df3e10fa
CodeGeek <khushbudadhe732@gmail.com>
2024-02-06 10:42:03 +0530

    Time: 20 ms (12.59%), Space: 45.1 MB (6.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,51 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length()!=t.length()){
+//             return false;
+//         }
+//         char arr1[]=s.toCharArray();
+//         char arr2[]=t.toCharArray();
+//         Arrays.sort(arr1);
+//         Arrays.sort(arr2);
+//         for(int i=0;i<arr1.length;i++){
+//             if(arr1[i]!=arr2[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
-            return false;
+        HashMap<Character,Integer>mp = new HashMap<>();
+        
+        for(int i = 0; i<s.length(); i++){
+            if(mp.containsKey(s.charAt(i))){
+                mp.put(s.charAt(i), mp.put(s.charAt(i),0)+1);
+            }
+            else{
+                mp.put(s.charAt(i),1);
+            }
         }
-        char arr1[]=s.toCharArray();
-        char arr2[]=t.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        for(int i=0;i<arr1.length;i++){
-            if(arr1[i]!=arr2[i]){
+        for(int i = 0; i<t.length(); i++){
+            if(mp.containsKey(t.charAt(i))){
+                mp.put(t.charAt(i), mp.get(t.charAt(i))-1);
+                
+            }
+            else{
+                return false;
+            }
+            
+        }
+       Set<Character> keys = mp.keySet();
+        // Loop over all keys and check if all keys are 0.
+        // If so it means it is anagram.
+        for (Character key : keys) {
+            if (mp.get(key) != 0) {
                 return false;
             }
         }
+        // Returning True as all keys are zero
         return true;
     }
 }
