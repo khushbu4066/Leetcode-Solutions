f960352f4692649b9784937fb14ff8e9743f376d
CodeGeek <khushbudadhe732@gmail.com>
2024-02-12 11:25:27 +0530

    Time: 1115 ms (5.60%), Space: 50.3 MB (58.77%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -18,22 +18,43 @@
 // }
 // O(n*n) which is note an opptimal solution
 
+// class Solution {
+//     public int majorityElement(int[] arr) {   
+        
+//         int n = arr.length;
+//         HashMap<Integer, Integer> ans = new HashMap<>();
+        
+//         for(int i = 0; i<n; i++){
+//             ans.put(arr[i],ans.getOrDefault(arr[i],0)+1);
+//         }
+        
+//         int c = -1;
+//         for(int j = 0; j<n; j++){
+//             if(ans.get(arr[j]) > n/2){
+//                 c = arr[j];
+//             }
+//         }
+//         return c;
+//     }
+// }
+
 class Solution {
     public int majorityElement(int[] arr) {   
         
         int n = arr.length;
-        HashMap<Integer, Integer> ans = new HashMap<>();
         
-        for(int i = 0; i<n; i++){
-            ans.put(arr[i],ans.getOrDefault(arr[i],0)+1);
-        }
-        
-        int c = -1;
-        for(int j = 0; j<n; j++){
-            if(ans.get(arr[j]) > n/2){
-                c = arr[j];
-            }
-        }
-        return c;
+       for(int i = 0; i<n; i++){
+           int count = 1;
+           for(int j = i + 1; j<n; j++){
+               if(arr[i] == arr[j]){
+                   count++;
+               }
+           }
+           if(count > n/2){
+               return arr[i];
+           }
+       }
+    
+        return -1;
     }
 }
