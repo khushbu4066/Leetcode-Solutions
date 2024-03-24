c81ed51baa407c12fde5cb0983df5cefa91bcbd0
CodeGeek <khushbudadhe732@gmail.com>
2024-03-24 10:43:15 +0530

    Time: 4 ms (85.61%), Space: 57 MB (87.09%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,32 +1,56 @@
+// class Solution {
+
+//     public int findDuplicate(int[] nums) {
+        
+// //         int slow=nums[0];
+// //         int fast= nums[0];
+        
+// //         do {
+// //             slow= nums[slow];
+// //             fast= nums[nums[fast]];
+// //         } while(slow!=fast);
+        
+// //         fast= nums[0];
+// //         while(slow!=fast) {
+// //             slow= nums[slow];
+// //             fast= nums[fast];
+// //         }
+// //         return slow;
+//         HashMap<Integer, Integer> mp = new HashMap<>();
+        
+//         int n = nums.length;
+//         for(int i = 0; i < n; i++){
+//             mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+//         }
+//         for(int i = 0; i<n; i++){
+//             if(mp.get(nums[i])>1){
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
 
     public int findDuplicate(int[] nums) {
         
-//         int slow=nums[0];
-//         int fast= nums[0];
+        int slow = nums[0];
+        int fast = nums[0];
         
-//         do {
-//             slow= nums[slow];
-//             fast= nums[nums[fast]];
-//         } while(slow!=fast);
+        slow = nums[slow];
+        fast = nums[nums[fast]];
         
-//         fast= nums[0];
-//         while(slow!=fast) {
-//             slow= nums[slow];
-//             fast= nums[fast];
-//         }
-//         return slow;
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        
-        int n = nums.length;
-        for(int i = 0; i < n; i++){
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
         }
-        for(int i = 0; i<n; i++){
-            if(mp.get(nums[i])>1){
-                return nums[i];
-            }
+        
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        return slow;
     }
 }
