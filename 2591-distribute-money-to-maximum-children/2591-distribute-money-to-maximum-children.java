d5d2a6394e967b1b7574fd4f13eccd4e663945bd
CodeGeek <khushbudadhe732@gmail.com>
2024-03-26 00:13:25 +0530

    Time: 2 ms (62.66%), Space: 41.8 MB (76.58%) - LeetHub

--- 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.java
+++ 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.java
@@ -37,27 +37,3 @@ class Solution {
         
     }
 }
-// class Solution{
-    
-// public int distMoney(int money, int children) {
-//         if(money < children) return -1;
-//         money -= children;
-
-//         int res = 0;
-//         while(money >= 7 && children > 0) {
-//             money -= 7;
-//             res++;
-//             children--;
-//         }
-//         if(res==0)
-//            return res;
-//         else
-//         {
-//             if(children==0 && money>0)
-//                res-=1;
-//             if(children==1 && money==3)
-//                res-=1;
-//         }
-//         return res;
-//     }
-// }
