0f47b57b374920dc249e6ac9ffbb0cedf43fe500
CodeGeek <khushbudadhe732@gmail.com>
2024-04-05 19:21:26 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (7.73%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -50,29 +50,59 @@
 // }
 //TLE
 
+
+
 // 2ND APPROACH
 //BOttom up 
+// class Solution {
+    
+//     public int rob(int[] nums) {
+
+//         int n = nums.length;
+//         int[] t = new int[n+1];
+
+//         if(n==1)
+//         return nums[0];
+
+//         t[0] = 0;
+//         t[1] = nums[0];
+        
+//         //t[i] = maximum stolen money till i houses
+
+//         for(int i = 2; i<=n; i++){
+            
+//             int steal = nums[i-1] + t[i-2];
+//             int skip = t[i-1];
+
+//             t[i] = Math.max(steal, skip);
+//         }
+//         return t[n];
+//     }
+// }
+
+
 class Solution {
     
+    int[] dp;
+    
+    int solve(int[] nums, int i, int n){
+        if(i>= n)
+            return 0;
+        
+        if(dp[i] != -1) return dp[i];
+        
+        int steal = nums[i] + solve(nums, i+2, n);
+        int skip = solve(nums, i+1, n);
+        
+        return dp[i] = Math.max(steal, skip);
+    }
     public int rob(int[] nums) {
-
         int n = nums.length;
-        int[] t = new int[n+1];
-
-        if(n==1)
-        return nums[0];
-
-        t[0] = 0;
-        t[1] = nums[0];
-
-        for(int i = 2; i<=n; i++){
-            
-            int steal = nums[i-1] + t[i-2];
-            int skip = t[i-1];
-
-            t[i] = Math.max(steal, skip);
-        }
-        return t[n];
+        
+        dp = new int[101];
+        Arrays.fill(dp, -1);
+        
+        return solve(nums, 0, n);
     }
 }
 
@@ -80,3 +110,4 @@ class Solution {
 
 
  
+
