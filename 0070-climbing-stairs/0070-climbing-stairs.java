07f63a1fc165edfd694d0f4ac5d776dd3ee395ea
CodeGeek <khushbudadhe732@gmail.com>
2024-04-05 13:53:00 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (96.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,99 @@
+// class Solution {
+//     public int climbStairs(int n) {
+        
+//         if(n==0 || n==1 || n==2)
+//             return n;
+
+//         int[] a = new int[n];
+//         a[0] = 1;
+//         a[1] = 2;
+
+//         for(int i = 2; i<n; i++){
+//          a[i]=a[i-1] + a[i-2];
+//         }
+//         return a[n-1];
+//     }
+// }
+
+
+// class Solution {
+//     public int climbStairs(int n) {
+        
+//         if(n==0 || n==1 || n==2)
+//             return n;
+        
+//         // a[0] = 0;
+//         // a[1] = 1;
+//         // a[2] = 2;
+        
+//         int a = 1;
+//         int b = 2; 
+//         int c = 3;
+
+//         // i ko nikalne le liye, you just need 1 phle and 2 phle - (i-1) and (i-2)
+//         for(int i = 3; i<=n; i++){
+//          c = b + a;
+            
+//             b = c;
+//             a = temp_b;
+//         }
+//         return c;
+//     }
+// }
+
+// // a = 0 (i-2)
+// // b= 1 (i-1)
+// // c = 2 (i)
+
+
+
+
+
+
+
+
+// class Solution {
+    
+//     int solve(int n){
+    
+//     if(n<0) return 0;
+//         if(n==0) return 1;
+        
+//         int one_step = solve(n-1);
+//     int two_step = solve(n-2);
+    
+//     return one_step + two_step;
+// }
+    
+//     public int climbStairs(int n) {
+        
+//        return solve(n);
+// }
+// }
+
+
 class Solution {
-    public int climbStairs(int n) {
-        if(n==1) return 1;
-
-        if(n==2) return 2;
-
-        int[] a = new int[n];
-        a[0] = 1;
-        a[1] = 2;
-
-        for(int i = 2; i<n; i++){
-         a[i]=a[i-1] + a[i-2];
-        }
-        return a[n-1];
-    }
+    
+    int[] dp;
+    
+    int solve(int n){
+    
+    if(n<0) return 0;
+    if(n == 0) return 1;
+        
+    if(dp[n] != -1) return dp[n];
+        
+        int one_step = solve(n-1);
+    int two_step = solve(n-2);
+    
+    return dp[n] = one_step + two_step;
 }
+    
+    public int climbStairs(int n) {
+        
+        dp = new int[n+1];
+        Arrays.fill(dp, -1);
+       return solve(n);
+}
+}
+
