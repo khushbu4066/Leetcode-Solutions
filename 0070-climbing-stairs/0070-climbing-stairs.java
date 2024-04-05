3d2d48e69708a8b5880a1584bdb4269fb0683808
CodeGeek <khushbudadhe732@gmail.com>
2024-04-05 14:07:40 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (26.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -72,28 +72,50 @@
 // }
 
 
+// class Solution {
+    
+//     int[] dp;
+    
+//     int solve(int n){
+    
+//     if(n<0) return 0;
+//     if(n == 0) return 1;
+        
+//     if(dp[n] != -1) return dp[n];
+        
+//         int one_step = solve(n-1);
+//     int two_step = solve(n-2);
+    
+//     return dp[n] = one_step + two_step;
+// }
+    
+//     public int climbStairs(int n) {
+        
+//         dp = new int[46];
+//         Arrays.fill(dp, -1);
+//        return solve(n);
+// }
+// }
+
+
 class Solution {
-    
-    int[] dp;
-    
-    int solve(int n){
-    
-    if(n<0) return 0;
-    if(n == 0) return 1;
-        
-    if(dp[n] != -1) return dp[n];
-        
-        int one_step = solve(n-1);
-    int two_step = solve(n-2);
-    
-    return dp[n] = one_step + two_step;
-}
     
     public int climbStairs(int n) {
         
-        dp = new int[46];
-        Arrays.fill(dp, -1);
-       return solve(n);
+       if( n == 0 || n == 1 || n == 2){
+           return n;
+       }
+        
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+        
+        for(int i = 3; i<=n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n];
+    }
 }
-}
+
 
