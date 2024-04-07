7a8262a42fc4aefb2056f921705393db3901ce0a
CodeGeek <khushbudadhe732@gmail.com>
2024-04-07 23:49:58 +0530

    Time: 1 ms (58.56%), Space: 40.6 MB (25.38%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -1,19 +1,37 @@
+// class Solution {
+//     public int totalMoney(int n) {
+//         int totalAmount = 0;
+//         int currDay = 1;
+         
+//         for(int i = 1; i <= n; i++){
+            
+//             totalAmount += currDay;
+//             currDay++;
+            
+//             if(i%7 == 0){
+//                 currDay = (i/7)+1;
+//             }
+//         }
+        
+//         return totalAmount;
+        
+//     }
+// }
+
 class Solution {
     public int totalMoney(int n) {
-        int totalAmount = 0;
-        int currDay = 1;
-         
-        for(int i = 1; i <= n; i++){
-            
-            totalAmount += currDay;
-            currDay++;
+        
+        int TA = 0;
+        int currday = 1;
+        for(int i = 1; i<=n; i++){
+            TA += currday;
+            currday++;
             
             if(i%7 == 0){
-                currDay = (i/7)+1;
+                currday = (i/7) + 1;
             }
         }
-        
-        return totalAmount;
+        return TA;
         
     }
 }
