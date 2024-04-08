356f860a4f3fee490718050db608dc418be7f5b5
CodeGeek <khushbudadhe732@gmail.com>
2024-04-08 08:51:34 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (95.65%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,29 +1,50 @@
+// class Solution {
+//     public int countStudents(int[] students, int[] sandwiches) {
+        
+//         int len = students.length;
+        
+//         Stack<Integer> st = new Stack<>();  // contains sanwitches
+//         Queue<Integer> q = new LinkedList<>();    // contains students
+        
+//         for(int i = 0; i<len; i++){
+//             st.push(sandwiches[len - i - 1]);
+//             q.add(students[i]);
+//         }
+        
+//         int lastServed = 0;
+//         while(!q.isEmpty() && lastServed < q.size()){
+//             if(st.peek() == q.peek()){
+//                 st.pop();
+//                 q.poll();
+//                 lastServed = 0;
+//             }
+//             else{
+//                 q.add(q.peek());
+//                 q.poll();
+//                 lastServed++;
+//             }
+//         }
+//         return q.size();
+//     }
+// }
+//TC = O(n)
+// SC = o(n)
+
+
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
         
-        int len = students.length;
+        int n = students.length;
+        int[] arr = new int[2];
         
-        Stack<Integer> st = new Stack<>();  // contains sanwitches
-        Queue<Integer> q = new LinkedList<>();    // contains students
-        
-        for(int i = 0; i<len; i++){
-            st.push(sandwiches[len - i - 1]);
-            q.add(students[i]);
+        for(int x : students){
+            arr[x]++;
         }
         
-        int lastServed = 0;
-        while(!q.isEmpty() && lastServed < q.size()){
-            if(st.peek() == q.peek()){
-                st.pop();
-                q.poll();
-                lastServed = 0;
-            }
-            else{
-                q.add(q.peek());
-                q.poll();
-                lastServed++;
-            }
+        for(int i = 0; i<n; i++){
+            if(arr[sandwiches[i]] == 0) return n-i;
+            arr[sandwiches[i]]--;
         }
-        return q.size();
+        return 0;
     }
 }
