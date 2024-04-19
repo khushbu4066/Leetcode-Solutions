fd28e29bb95c7287471e39bfd2b710fe0b78b344
CodeGeek <khushbudadhe732@gmail.com>
2024-04-19 16:46:13 +0530

    Time: 5 ms (31.54%), Space: 48.9 MB (89.54%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,50 +1,105 @@
-class Solution {
+// class Solution {
     
-    private List<int[]> dir = new ArrayList<>();
+//     private List<int[]> dir = new ArrayList<>();
     
-    Solution(){
+//     Solution(){
         
-        dir.add(new int[]{1, 0});
-        dir.add(new int[]{-1, 0});
-        dir.add(new int[]{0, 1});
-        dir.add(new int[]{0, -1});
+//         dir.add(new int[]{1, 0});
+//         dir.add(new int[]{-1, 0});
+//         dir.add(new int[]{0, 1});
+//         dir.add(new int[]{0, -1});
+//     }
+    
+    
+//     private void DFS(char[][] grid, int i, int j){
+        
+//         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1')
+//             return;
+        
+        
+//         grid[i][j] = '$';
+        
+//         for(int[] p : dir ){
+//             int i_ = i + p[0];
+//             int j_ = j + p[1];
+//             DFS(grid, i_, j_);
+//         } 
+//     }
+    
+    
+//     public int numIslands(char[][] grid) {
+        
+//         if(grid.length == 0){
+//             return 0;
+//         }
+        
+//         int m = grid.length;
+//         int n = grid[0].length;
+//         int islands = 0;
+        
+//         for(int i = 0; i<m; i++){
+//             for(int j = 0; j<n; j++){
+//                 if(grid[i][j] == '1'){
+//                     DFS(grid, i, j);
+//                     islands += 1;
+//                 }
+//             }
+//         }
+//         return islands;
+//     }
+// }
+
+
+//Approach (BFS & DFS)
+//T.C : O(m*n)
+//S.C : O(m*n)
+
+
+
+class Solution {
+    private int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
+    private boolean isSafe(char[][] grid, int i, int j) {
+        return i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == '1';
     }
     
-    
-    private void DFS(char[][] grid, int i, int j){
-        
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1')
-            return;
-        
-        
+    private void bfs(char[][] grid, int i, int j, Queue<int[]> que) {
+        que.offer(new int[]{i, j});
         grid[i][j] = '$';
         
-        for(int[] p : dir ){
-            int i_ = i + p[0];
-            int j_ = j + p[1];
-            DFS(grid, i_, j_);
-        } 
-    }
-    
-    
-    public int numIslands(char[][] grid) {
-        
-        if(grid.length == 0){
-            return 0;
-        }
-        
-        int m = grid.length;
-        int n = grid[0].length;
-        int islands = 0;
-        
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(grid[i][j] == '1'){
-                    DFS(grid, i, j);
-                    islands += 1;
+        while (!que.isEmpty()) {
+            int[] curr = que.poll();
+            
+            for (int[] p : dir) {
+                int i_ = curr[0] + p[0];
+                int j_ = curr[1] + p[1];
+                
+                if (isSafe(grid, i_, j_)) {
+                    que.offer(new int[]{i_, j_});
+                    grid[i_][j_] = '$';
                 }
             }
         }
-        return islands;
+    }
+    
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0)
+            return 0;
+        
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+        Queue<int[]> que = new ArrayDeque<>();
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    bfs(grid, i, j, que);
+                    count++;
+                }
+            }
+        }
+        
+        return count;
     }
 }
