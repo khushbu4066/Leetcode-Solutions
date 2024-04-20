860a891d5fdace1f8685aeebe5e57729808f672c
CodeGeek <khushbudadhe732@gmail.com>
2024-04-20 21:47:02 +0530

    Time: 1 ms (99.42%), Space: 57.4 MB (13.06%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -48,6 +48,32 @@
 // }
 
 // Optimal approach using 1 loop
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+        
+//         int n = nums.length;
+        
+//         int maxSum = Integer.MIN_VALUE ;
+//         int sum = 0;
+        
+//         for(int i = 0; i<n; i++){
+//                 sum += nums[i];
+               
+//             if(sum > maxSum){
+//                 maxSum = sum;
+//             }
+//             if(sum < 0){
+//                 sum = 0;
+//             }
+//         }
+        
+//         return maxSum;
+//     }
+// }
+
+
+//Kadane algo
+
 class Solution {
     public int maxSubArray(int[] nums) {
         
@@ -56,11 +82,19 @@ class Solution {
         int maxSum = Integer.MIN_VALUE ;
         int sum = 0;
         
+        int start = 0;
+        int ansStart = -1, ansEnd = -1;
+        
         for(int i = 0; i<n; i++){
+            
+            if(sum == 0) start = i;
                 sum += nums[i];
                
             if(sum > maxSum){
                 maxSum = sum;
+                
+                ansStart = start;
+                ansEnd = i;
             }
             if(sum < 0){
                 sum = 0;
