6ae24d5377c440a780570d16a4adb19b62677bdf
CodeGeek <khushbudadhe732@gmail.com>
2024-04-20 21:32:22 +0530

    Time: 1 ms (99.42%), Space: 56.8 MB (76.50%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,72 @@
+// Naive approave
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+        
+//         int n = nums.length;
+        
+//         int maxSum = Integer.MIN_VALUE ;
+        
+//         for(int i = 0; i<n; i++){
+//             for(int j = i; j<n; j++){
+//                 int sum = 0;
+                
+//              for(int k = i; k<=j; k++){
+//                  sum += nums[k];
+//              }
+                
+//                maxSum = Math.max(maxSum, sum);
+             
+                
+//             }
+//         }
+        
+//         return maxSum;
+//     }
+// }
+
+// Better
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+        
+//         int n = nums.length;
+        
+//         int maxSum = Integer.MIN_VALUE ;
+        
+//         for(int i = 0; i<n; i++){
+//             int sum = 0;
+//             for(int j = i; j<n; j++){
+//                 sum += nums[j];
+//                maxSum = Math.max(maxSum, sum);
+                
+//             }
+//         }
+        
+//         return maxSum;
+//     }
+// }
+
+// Optimal approach using 1 loop
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int n = nums.length;
+        
+        int maxSum = Integer.MIN_VALUE ;
+        int sum = 0;
+        
+        for(int i = 0; i<n; i++){
+                sum += nums[i];
+               
+            if(sum > maxSum){
+                maxSum = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        
+        return maxSum;
+    }
+}
