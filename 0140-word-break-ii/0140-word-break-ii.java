979b3b1de56a548e0a07fe47e93e19650ddc3ebd
CodeGeek <khushbudadhe732@gmail.com>
2024-05-25 18:53:21 +0530

    Time: 1 ms (94.29%), Space: 41.6 MB (68.54%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        
+        Set<String> wordSet = new HashSet<>(wordDict);
+        List<String> results = new ArrayList<>();
+        backtrack(s, wordSet, new StringBuilder(), results, 0);
+        return results;
+    }
+
+    private static void backtrack(
+            String s,
+            Set<String> wordSet,
+            StringBuilder currentSentence,
+            List<String> results,
+            int startIndex) {
+        // when the index goes to the all string, that means all words found and no letter left, add it to the result string.
+        if (startIndex == s.length()) {
+            results.add(currentSentence.toString().trim());
+            return;
+        }
+
+        for (int endIndex = startIndex + 1; endIndex <= s.length(); endIndex++) {
+            String word = s.substring(startIndex, endIndex);
+            if (wordSet.contains(word)) {
+                int currentLength = currentSentence.length();
+                currentSentence.append(word).append(" ");
+                backtrack(s, wordSet, currentSentence, results, endIndex);
+                currentSentence.setLength(currentLength);
+            }
+        }
+    }
+}
