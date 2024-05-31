77e318f1ef4c27422f21db41134115bb4122b4d4
CodeGeek <khushbudadhe732@gmail.com>
2024-05-31 08:43:19 +0530

    Time: 1 ms (100.00%), Space: 45.4 MB (69.87%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -26,27 +26,71 @@
 public class Solution {
     public int[] singleNumber(int[] nums) {
         // Step 1: Get the XOR of the two unique numbers
-        int xorResult = 0;
-        for (int num : nums) {
-            xorResult ^= num;
-        }
+//         int xorResult = 0;
+//         for (int num : nums) {
+//             xorResult ^= num;
+//         }
 
-        // Step 2: Find the rightmost set bit in the XOR result
+//         // Step 2: Find the rightmost set bit in the XOR result
+//         int rightmostSetBit = 1;
+//         while ((rightmostSetBit & xorResult) == 0) {
+//             rightmostSetBit <<= 1;
+//         }
+
+//         // Step 3: Divide the numbers into two groups based on the rightmost set bit
+//         int group1 = 0, group2 = 0;
+//         for (int num : nums) {
+//             if ((num & rightmostSetBit) == 0) {
+//                 group1 ^= num;
+//             } else {
+//                 group2 ^= num;
+//             }
+//         }
+
+//         return new int[]{group1, group2};
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        int x = 0;
+        for(int num:nums){
+            x = x^num;
+        }
         int rightmostSetBit = 1;
-        while ((rightmostSetBit & xorResult) == 0) {
+        while((rightmostSetBit&x)==0){
             rightmostSetBit <<= 1;
         }
-
-        // Step 3: Divide the numbers into two groups based on the rightmost set bit
         int group1 = 0, group2 = 0;
-        for (int num : nums) {
-            if ((num & rightmostSetBit) == 0) {
+        for(int num: nums){
+            if((num & rightmostSetBit)==0){
                 group1 ^= num;
-            } else {
+            }
+            else{
                 group2 ^= num;
             }
         }
-
-        return new int[]{group1, group2};
+        return new int[] {group1, group2};
     }
 }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
